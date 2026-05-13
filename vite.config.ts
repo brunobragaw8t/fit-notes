9bb3a024eaaf10365d5e3d631108c6bf819ea246
Bruno Braga <brunobragaw8t@gmail.com>
2026-05-13 12:00:20 +0100

    chore: install react-scan

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import reactScan from "@react-scan/vite-plugin-react-scan";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  plugins: [react(), reactScan(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
 });
