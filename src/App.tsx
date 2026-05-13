00d06dfab08f4df73ddb6b1aa71598b12e0be0ca
Bruno Braga <brunobragaw8t@gmail.com>
2026-05-13 15:33:50 +0100

    chore: add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-  return <h1>Hello, world!</h1>;
+  return (
+    <>
+      <h1>Hello, world!</h1>
+      <Button onClick={() => alert("Hello!")}>Click me</Button>
+    </>
+  );
 }
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,14 @@ import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import reactScan from "@react-scan/vite-plugin-react-scan";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), reactScan(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
