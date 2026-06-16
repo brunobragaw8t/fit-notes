afda56fc0a841838a2bf92e26395b3bd71ed2ba7
Bruno Braga <brunobragaw8t@gmail.com>
2026-06-16 21:15:45 +0100

    fix: change app height from vh to dvh

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,7 +3,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createRootRoute({
   component: () => (
-    <div className="flex h-screen flex-col">
+    <div className="flex h-dvh flex-col">
       <main className="flex-1 overflow-auto p-4">
         <Outlet />
       </main>
