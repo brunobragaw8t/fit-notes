40aaab1ce04c0db754ef811911e94fea96713cb9
Bruno Braga <brunobragaw8t@gmail.com>
2026-06-17 20:23:22 +0100

    feat: enable offline mode

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import babel from "@rolldown/plugin-babel";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -14,6 +15,35 @@ export default defineConfig({
     reactScan(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
+    VitePWA({
+      workbox: {
+        globPatterns: ["**/*"],
+      },
+      includeAssets: ["**/*"],
+      manifest: {
+        name: "Fit Notes",
+        short_name: "Fit Notes",
+        icons: [
+          {
+            src: "/favicon.svg",
+            type: "image/svg+xml",
+            sizes: "512x512",
+          },
+          {
+            src: "/icon-512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+          {
+            src: "/icon-192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+        ],
+        start_url: "/",
+        display: "standalone",
+      },
+    }),
   ],
   resolve: {
     alias: {
